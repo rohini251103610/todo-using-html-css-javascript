f5ab303e9810ed00a80ee71a3243f92de7bad1cf
Rohini.R <rohinirachelr@gmail.com>
2024-07-21 14:36:48 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
-function remove(){
-    let dele=document.getElementsByTagName("input")[1];
-    let btn=document.getElementById("del");
-    dele.remove();
-    btn.remove();
+function getinput(){
+    var add=document.getElementById("listtask")
+    var input=document.getElementById("myinput")
+        var litem=document.createElement("li")
+        litem.innerHTML=input.value+""+"<button onclick='del(event)'>Delete</button>"
+        if(input.value==null||input.value==""){
+            alert("Enter some text");
+        }
+        else{
+        add.append(litem);
+        }
+}
+
+function del(event){
+    event.target.parentElement.remove();
 }
